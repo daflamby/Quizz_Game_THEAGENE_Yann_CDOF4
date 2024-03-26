fa22f226e87cb23e9e6ebc399cb8a12b63688343
daflamby <157586163+daflamby@users.noreply.github.com>
2024-03-26 17:52:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='QuizGame-Lily_LM-CDOF2',
+ version='1.0',
+ author='Lily LA MORELLA',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
